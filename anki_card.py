efb4d68017a92f5b50e1120a07a76fb2a47b6187
jj-stigell <jj.stigell@gmail.com>
2021-08-26 20:24:44 +0300

    added data to cvs

--- anki_card.py
+++ anki_card.py
@@ -2,11 +2,9 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 
-
 def main():
 
     driver = webdriver.Chrome("/home/jj-stigell/Downloads/chromedriver") #path to chromedriver, temp for now
-
     sentences_jpn = []  # sentence in japanese
     sentences_eng = []  # sentence in english
     grammar = []  # grammar point
@@ -22,19 +20,8 @@ def main():
         sentences_jpn.append(sentence_jpn.text)
         sentences_eng.append(sentence_eng.text)
 
-
-
-
-
-
-
-
-
-
-
-
-
+    df = pd.DataFrame({'Expression': sentences_jpn, 'Meaning': sentences_eng})
+    df.to_csv('products.csv', index=False, encoding='utf-8')
 
 
 main()
-
